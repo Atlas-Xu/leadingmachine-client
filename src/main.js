9a7cc37c2660a55e6e7c1f03344401d12f7528a3
Atlas-Xu <34097133+Atlas-Xu@users.noreply.github.com>
2022-03-25 16:03:19 +0800

    face-detection

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import './assets/css/global.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
