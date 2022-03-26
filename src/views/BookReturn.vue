d72ed8b5a6c22f0eb428dd5fb93e0fd0a4b18113
Atlas-Xu <34097133+Atlas-Xu@users.noreply.github.com>
2022-03-26 11:09:58 +0800

    login

--- src/views/BookReturn.vue
+++ src/views/BookReturn.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="login_container">
     还书
   </div>
 </template>
@@ -11,5 +11,11 @@ export default {
 </script>
 
 <style scoped>
-
+.login_container {
+  height: 100vh;
+  background-image: url("../assets/img/login_bg.jpg");
+  background-size: 100% 100%;
+  background-color: #7a7a7a;
+  background-blend-mode: multiply;
+}
 </style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,40 @@
 <template>
-  <div>
-    <h1>This is an login page</h1>
+  <div class="login_container">
+    <el-main>
+      <el-card shadow="always" class="box-card">
+        <div
+            style="
+        margin: 0px 0px 15px 0px;
+        color: #67c23a;
+        font-weight: 600;
+        font-size: 15px;
+
+      "
+        >
+          人脸识别身份验证登录
+        </div>
+        <div>
+          【温馨提示：请保持光线充足，并正对摄像头】
+        </div>
+      </el-card>
+    </el-main>
   </div>
 </template>
+
+<style scoped>
+.login_container {
+  height: 100vh;
+  background-image: url("../assets/img/login_bg.jpg");
+  background-size: 100% 100%;
+  background-color: #7a7a7a;
+  background-blend-mode: multiply;
+}
+.box-card{
+  height: 600px;
+  width: 650px;
+  position: absolute;
+  left: 33%;
+  top: 20%;
+}
+
+</style>
